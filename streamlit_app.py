73d521da70f22fe10328a00af387c2fbc87eb2c0
arvind4580 <142278462+arvind4580@users.noreply.github.com>
2023-08-15 08:36:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import streamlit
 streamlit.title('My parents new healthy dinner')
 
 streamlit.header('Breakfast Menu')
-
-streamlit.text('Bread Butter')
-streamlit.text('Cereal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
