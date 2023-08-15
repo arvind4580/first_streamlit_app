6c979153ae75b4f39933ecb005cc5e58f0759a06
arvind4580 <142278462+arvind4580@users.noreply.github.com>
2023-08-15 08:39:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 🍞 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 🥑Hard-Boiled Free-Range Egg')
+
+streamlit.header('Lunch Menu')
